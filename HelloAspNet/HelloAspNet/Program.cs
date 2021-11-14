8d219ceb5dbe36bad54dad0e256d7930d929264d
Student1 <Student1@DESKTOP-UGHUNVE>
2021-11-14 11:48:52 +0300

    проба

--- HelloAspNet/HelloAspNet/Program.cs
+++ HelloAspNet/HelloAspNet/Program.cs
@@ -1,6 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => DateTime.Now.ToString("dddd , MMM dd yyyy,hh:mm:ss"));
+
+var yers = DateTime.Now.Year;
+int temp;
+
+if (DateTime.IsLeapYear(yers))
+{
+    temp = 366 - DateTime.Now.DayOfYear;
+}
+else
+{
+    temp = 365 - DateTime.Now.DayOfYear;
+}
+
+
+app.MapGet("/", () => "Дней до нового года:" + " " +  temp);
 
 app.Run();
